6ed619ac966c013ea203a764a9c386fbf04668d0
iamarasekera <iamarasekera.dev@gmail.com>
2023-08-14 15:38:27 +0800

    Created a dummy home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,20 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import { Box, Typography, Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+    <Box>
+      <Typography variant='h4' component='h1'>Hello</Typography>
+      <Typography variant='body1' component='p'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci sint odit repellat dignissimos asperiores quidem repudiandae necessitatibus dicta facere nulla non quaerat obcaecati, qui vero fuga assumenda sed iure amet in voluptate earum? Ipsa adipisci quam ab perspiciatis excepturi totam quisquam voluptatum fuga, corrupti suscipit id, consequatur eligendi soluta quasi. Nulla sit porro rem in inventore, ullam facilis repellat ut ipsum et nihil beatae, velit commodi! Expedita laborum cumque dolore autem blanditiis non perferendis molestiae doloribus ipsum sed enim placeat qui vitae voluptates dolores, cum deleniti iusto magnam iure illo at, inventore laudantium. Itaque ex, aut tenetur molestiae distinctio maxime.</Typography>
+    </Box>
+    <Box>
+      <Typography variant='h4' component='h1'>Second section</Typography>
+      <Typography variant='body1' component='p'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci sint odit repellat dignissimos asperiores quidem repudiandae necessitatibus dicta facere nulla non quaerat obcaecati, qui vero fuga assumenda sed iure amet in voluptate earum? Ipsa adipisci quam ab perspiciatis excepturi totam quisquam voluptatum fuga, corrupti suscipit id, consequatur eligendi soluta quasi. Nulla sit porro rem in inventore, ullam facilis repellat ut ipsum et nihil beatae, velit commodi! Expedita laborum cumque dolore autem blanditiis non perferendis molestiae doloribus ipsum sed enim placeat qui vitae voluptates dolores, cum deleniti iusto magnam iure illo at, inventore laudantium. Itaque ex, aut tenetur molestiae distinctio maxime.</Typography>
+    </Box>
+    <Button variant='contained' color='warning'>Im a button</Button>
+    <Button variant='outlined'>Im a button</Button>
+    <Button variant='text'>Im a button</Button>
+  </>
   )
 }
