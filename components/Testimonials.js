1bf2cb682b9fc12ccedb3dbd8a7fd290b3ae72e2
TH101 <thiliniherath109@gmail.com>
2023-08-17 12:08:29 +0530

    Added testimonial section to the home page

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,72 +1,142 @@
-import { Box, Grid, Typography } from '@mui/material';
-import React from 'react'
+"use client";
+import {
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Rating,
+  Typography,
+} from "@mui/material";
+import React from "react";
 import Image from "next/image";
-import review from "../public/images/Review.png";
+import profile from "../public/images/Profile.png";
 
 const Testimonials = () => {
-    return (
-      <>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <Box
+  const [value, setValue] = React.useState(4);
+  const userAvatarStyle = {
+    borderRadius: "50%",
+    width: "63.364px",
+    height: "63.364px",
+    objectFit: "cover",
+  };
+
+  const accountStatsStyle = {
+    // textAlign: "center",
+    marginTop: "31.682px", // Half of the userAvatar height
+    width: "90%",
+    height: "auto",
+    // background: "#e0e0e0",
+    // boxShadow: "9px 9px 10px rgba(0, 0, 0, 0.25)",
+    borderRadius: "18px",
+    paddingTop: "36px",
+    marginLeft: "5%",
+  };
+
+  const accountContainerStyle = {
+    position: "relative",
+    top: "15%",
+    // transform: "translateY(30%)",
+  };
+
+  const userAvatarContainerStyle = {
+    position: "absolute",
+    top: "-31.682px", // Half of the userAvatar height
+    left: "calc(12% - 31.682px)", // Half of the userAvatar width
+    padding: "8px",
+    borderRadius: "50%",
+    backgroundColor: "#fff",
+  };
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Box style={accountContainerStyle}>
+            <Box style={userAvatarContainerStyle}>
+              <Image src={profile} alt="Avatar" style={userAvatarStyle} />
+            </Box>
+            <Card elevation={0} style={accountStatsStyle}>
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  style={{
+                    color: "#0C111F",
+                    fontFamily: "Montserrat, sans-serif",
+                    fontSize: "24px",
+                    lineHeight: "28px",
+                    fontStyle: "normal",
+                    fontWeight: "700",
+                    // marginTop: "10px",
+                  }}
+                >
+                  Jhone Lown
+                </Typography>
+                <Rating name="read-only" value={value} readOnly />
+                <Typography
+                  variant="body2"
+                  style={{
+                    color: "#0C111F",
+                    fontFamily: "Inter",
+                    fontSize: "18px",
+                    lineHeight: "180%",
+                    fontStyle: "normal",
+                    fontWeight: "400",
+                    letterSpacing: "-0.18px",
+                    // marginTop: "8px",
+                  }}
+                >
+                  “A very pleasant experience, plus the service is very
+                  friendly, recommended for those who want to try!”
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} paddingRight={{ xs: "16px", md: "32px" }}>
+          <Box>
+            <Typography
+              variant="h1"
               style={{
-                position: "relative",
-                maxWidth: "400px",
-                margin: "0 auto",
+                color: "#0C111F",
+                fontFamily: "Inter",
+                fontSize: "48px",
+                // fontSize: { xs: "36px", md: "48px" },
+                lineHeight: "130%",
+                fontStyle: "normal",
+                letterSpacing: "-0.48px",
+                fontWeight: "700",
+                marginTop: "95px",
+                marginLeft: "30px",
+                // marginTop: { xs: "40px", md: "90px" },
+                // marginLeft: { xs: "16px", md: "70px" },
+                // textAlign: { xs: "center", md: "left" },
               }}
             >
-              <Image
-                src={review}
-                alt="test"
-                width={"558px"}
-                height={"238px"}
-                style={{
-                  width: "100%",
-                  height: "auto",
-                  borderRadius: "20px",
-                  marginTop: "80px",
-                  //   flexShrink: 0,
-                }}
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={12} md={6} paddingRight={{ xs: "16px", md: "32px" }}>
-            <Box>
-              <Typography
-                variant="h1"
-                style={{
-                  color: "#0C111F",
-                  fontFamily: "Inter",
-                  fontSize: "48px",
-                  lineHeight: "130%",
-                  fontStyle: "normal",
-                  letterSpacing: "-0.48px",
-                  fontWeight: "700",
-                  marginTop: "90px",
-                }}
-              >
-                Testimonials Travala Customers
-              </Typography>
-              <Typography
-                variant="body2"
-                style={{
-                  color: "#0C111F",
-                  fontFamily: "Inter",
-                  fontSize: "20px",
-                  lineHeight: "180%",
-                  fontStyle: "normal",
-                  fontWeight: "400",
-                  marginTop: "5px",
-                  marginBottom: "90px",
-                }}
-              >
-                What our customers have to say about our service, check below!
-              </Typography>
-            </Box>
-          </Grid>
+              Testimonials Travala Customers
+            </Typography>
+            <Typography
+              variant="body2"
+              style={{
+                color: "#0C111F",
+                fontFamily: "Inter",
+                fontSize: "20px",
+                lineHeight: "180%",
+                fontStyle: "normal",
+                fontWeight: "400",
+                marginTop: "5px",
+                marginBottom: "95px",
+                marginLeft: "30px",
+                // marginLeft: { xs: "16px", md: "70px" },
+                // textAlign: { xs: "center", md: "left" },
+              }}
+            >
+              What our customers have to say about our service, check below!
+            </Typography>
+          </Box>
         </Grid>
-      </>
-    );
-}
+      </Grid>
+    </>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
