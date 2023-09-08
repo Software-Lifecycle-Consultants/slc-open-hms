46374e0bbffa945e6977fdd83c05af34eaa4c411
iamarasekera <iamarasekera.dev@gmail.com>
2023-09-08 22:48:49 +0800

    Removed unnecessary imports and cleaned the code

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/navigation";
-import CheckoutScreen from "@/pages/CheckoutScreen";
-import ExploreScreen from "@/pages/ExploreScreen";
 import HomeScreen from "@/pages/HomeScreen";
-import RoomDetailsScreen from "@/pages/RoomDetailsScreen";
 
 export default function Home() {
-  const router = useRouter();
-
-  const navigationToHomeScreen = () => {
-    router.push("/HomeScreen");
-  };
   return (
     <>
-      {/* <button onClick={navigationToHomeScreen}>Go to home</button> */}
       <HomeScreen />
-      {/* <CheckoutScreen />  */}
-      {/* <RoomDetailsScreen /> */}
-      {/* <ExploreScreen /> */}
     </>
   );
 }
