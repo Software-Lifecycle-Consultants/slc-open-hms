5b901ba3248bc3b2c44d5d8eb9942db4a93d1fa6
iamarasekera <iamarasekera.dev@gmail.com>
2023-09-08 22:50:54 +0800

    Add the CssBaseline component to override the 8px margin

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
+import { CssBaseline } from '@mui/material';
 import HomeScreen from "@/pages/HomeScreen";
 
 export default function Home() {
   return (
     <>
+      {/* Add the CssBaseline component to override the 8px margin */}
+      <CssBaseline />
       <HomeScreen />
     </>
   );
