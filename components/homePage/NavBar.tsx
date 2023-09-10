99e93899c693e6e2212879e4b16d7402311d579f
iamarasekera <iamarasekera.dev@gmail.com>
2023-09-11 00:20:17 +0800

    Added routing to contact us page

--- components/homePage/NavBar.tsx
+++ components/homePage/NavBar.tsx
@@ -24,7 +24,7 @@ const pages = [
   { label: "Home", route: "/" },
   { label: "Explore", route: "/ExploreScreen" },
   { label: "Book Now", route: "/CheckoutScreen" },
-  { label: "Contact", route: "/ContactScreen" },
+  { label: "Contact", route: "/contact_us" },
 ];
 
 const NavBar = () => {
