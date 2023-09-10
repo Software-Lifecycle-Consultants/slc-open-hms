cb31fad491a712feb85fd5dcd7d04dd3a106ff2b
iamarasekera <iamarasekera.dev@gmail.com>
2023-09-11 00:02:16 +0800

    Reset the root layout to default to add any goabally shared UIs in the future

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,11 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import CssBaseline from '@mui/material/CssBaseline'; // Import the CssBaseline component
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'HMS Dev',
-  description: 'HMS System under development',
-}
-
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {/* Add the CssBaseline component to override the 8px margin */}
-        {/* <CssBaseline /> */}
-        {children}
-      </body>
-    </html>
-  )
-}
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
