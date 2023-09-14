e33bca964e94b955329d87a6950179f6ef1029ff
TH101 <thiliniherath109@gmail.com>
2023-09-14 10:20:33 +0530

    Added register and login pages

---
+++ pages/Register.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Button, TextField, Typography, Container, Grid } from "@mui/material";
+
+const Register: React.FC = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: string
+  ) => {
+    setFormData({
+      ...formData,
+      [field]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Handle form submission here, e.g., send data to the server
+    console.log(formData);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center">
+              Register
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Username"
+              variant="outlined"
+              value={formData.username}
+              onChange={(e) => handleChange(e, "username")}
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              type="email"
+              value={formData.email}
+              onChange={(e) => handleChange(e, "email")}
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Password"
+              variant="outlined"
+              type="password"
+              value={formData.password}
+              onChange={(e) => handleChange(e, "password")}
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default Register;
