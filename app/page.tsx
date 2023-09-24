23be9812900732362f57cea0f8995c09c1886959
iamarasekera <iamarasekera.dev@gmail.com>
2023-09-24 17:56:13 +0800

    Added the index file content to the page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,70 @@
+import { Box, Container } from "@mui/material";
+import Banner from "../components/homePage/Banner";
+import Testimonials from "../components/homePage/Testimonials";
+import Blog from "../components/homePage/Blog";
+import CustomCard from "../components/homePage/CustomCard";
+import LogoCarousel from "@/components/homePage/LogoCarousel";
+
+/**
+ * HomeScreen component represents the main screen of the website.
+ * It includes sections like Banner, Destination Card, Blog, Testimonials, and Footer.
+ */
+
+/* Style class for the box component of each section component */
+const sectionStyle = {
+  backgroundColor: "#FFF",
+  padding: "20px 0",
+  width: "100%",
+};
+
+
+export default function HomeScreen() {
+  return (
+    <>
+      {/* Banner Section */}
+      <Box
+        sx={{
+          backgroundColor: "#0C111F",
+          color: "white",
+          padding: "40px 0",
+          flexGrow: 1,
+        }}
+      >
+        <Container>
+          <Banner />
+        </Container>
+      </Box>
+
+      {/* Destination Card */}
+      <Box sx={sectionStyle}>
+        <Container>
+          <CustomCard />
+        </Container>
+      </Box>
+
+      {/* Blog */}
+      <Box sx={sectionStyle}>
+        <Container>
+          <LogoCarousel />
+        </Container>
+      </Box>
+
+      {/* Blog */}
+      <Box sx={sectionStyle}>
+        <Container>
+          <Blog />
+        </Container>
+      </Box>
+
+      {/* Testimonials */}
+      <Box
+        sx={{sectionStyle,backgroundColor: "#F7F7F7",}}
+      >
+        <Container>
+          <Testimonials />
+        </Container>
+      </Box>
+
+    </>
+  );
+}
