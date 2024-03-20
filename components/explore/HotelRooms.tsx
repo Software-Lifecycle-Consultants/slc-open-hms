247800cb00247edfcae3b976a52a3df3a7bfb0e6
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-03-20 13:08:59 +0530

    Add changers based on search freture

--- components/explore/HotelRooms.tsx
+++ components/explore/HotelRooms.tsx
@@ -3,14 +3,22 @@ import React from 'react'
 import RoomCard from '../guestRoomDetails/RoomCard';
 import { roomDetails } from '../../data/explorePage'
 
-const HotelRooms = () => {
+const HotelRooms = ({query} : {query: string}) => {
+
+  
+
+  const filteredRooms = roomDetails.filter((room) => {        //filterd the rooms based on search query by room name 
+    return room.roomName.toLowerCase().includes(query.toLowerCase());
+  });
+
+  
   return (
     <>
       <Grid container spacing={2}>
-        {roomDetails.map((item) => (
+        {roomDetails && filteredRooms.map((item) => (
           <Grid key={item.id} item xs={12} sm={6} md={4}>
             <RoomCard
-              image={item.image}
+              image={item.roomCoverImage ? item.roomCoverImage : ''}
               roomName={item.roomName}
               price={item.price}
             />
