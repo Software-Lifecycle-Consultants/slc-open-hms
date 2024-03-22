08f6f57cbb5d89b84937d43ed9d43b999aab09fb
chithraka99 <chithrakanayana99@gmail.com>
2024-03-22 10:54:05 +0530

    updated privacypolicyPage

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,182 +1,102 @@
+import { bodysectio5, bodysection1, bodysection102, bodysection2, bodysection202, bodysection3, bodysection302, bodysection4, bodysection402, bodysection501, bodysection6, bodysection7, header1Details } from "@/data/privacypolicyPage";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "HMS Dev | Privacy Policy",
   description: "HMS System under development",
 };
+
 export default function PrivacyPolicy() {
   return (
     <>
-   <div style={{
-      maxWidth: '1220px', /* Adjust max-width as needed */
-      margin: '0 auto', /* Center the container */
-      padding: '40px' /* Add padding */
-    }}>
-      <h1>Privacy Policy</h1>
-      <h4>
-        Effective Date: <b>May 8, 2023</b>
-      </h4>
-      <p>
-        This Privacy Policy outlines how SLC (&#8220;we,&#8221;&#8220;us,&#8221;
-        or &#8220;our&#8221;) collects, uses, and safeguards the personal
-        information of visitors (&#8220;you&#8221; or &#8220;your&#8221;) to our
-        website. We are committed to protecting your privacy and complying with
-        applicable data protection regulations, including the General Data
-        Protection Regulation (GDPR) and other relevant laws. By accessing our
-        website or using our services, you consent to the practices described in
-        this Privacy Policy.
-      </p>
+      <div
+        style={{
+          maxWidth: "1220px" /* Adjust max-width as needed */,
+          margin: "0 auto" /* Center the container */,
+          padding: "40px" /* Add padding */,
+        }}>
+        <h1>{header1Details.title}</h1>
+        <h4>
+          Effective Date: <b>{header1Details.effectiveDate}</b>
+        </h4>
+        <p>{header1Details.description}</p>
 
-      <h2>1. Information We Collect</h2>
-      <p>
-        1.1 Personal Information:
-        <br />
-        We may collect personal information directly from you when you interact
-        with our website, subscribe to our newsletter, or use our &#8220;Contact
-        Us&#8221; form. The types of personal information we may collect
-        include:
-      </p>
+        <h2>{bodysection1.title}</h2>
+        <p>
+          {bodysection1.subtitle}
+          <br />
+        </p>
 
-      <p>
-        &#8211; Contact information (such as name, email address, phone number)
-        <br />
-        &#8211; Company information (such as company name, job title)
-        <br />
-        &#8211; Communication preferences
-      </p>
-      <p>
-        1.2 Automatically Collected Information:
-        <br />
-        When you visit our website, we may automatically collect certain
-        information about your device, browsing activities, and usage patterns,
-        including:
-      </p>
-      <p>
-        &#8211; IP address
-        <br />
-        &#8211; Browser type and version
-        <br />
-        &#8211; Operating system
-        <br />
-        &#8211; Referral source
-        <br />
-        &#8211; Pages visited and time spent on our website
-      </p>
+        {bodysection1.content.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+        
+        <p>
+          {bodysection102.subtitle}
+        </p>
 
-      <h2>2. Use of Information</h2>
-      <p>
-        2.1 Providing and Improving Services:
-        <br />
-        We use the collected information to provide you with our software
-        consultancy services and improve your experience with our website. This
-        includes:
-      </p>
-      <p>
-        &#8211; Responding to your inquiries and providing requested information
-        <br />
-        &#8211; Sending newsletters and marketing communications (with your
-        consent)
-        <br />
-        &#8211; Analyzing website usage and improving our website&#8217;s
-        functionality and content
-        <br />
-        &#8211; Enhancing our services and tailoring them to your needs
-        <br />
-        &#8211; Managing and administering our business operations
-      </p>
+        {bodysection102.content.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
 
-      <p>
-        2.2 Legal Compliance:
-        <br />
-        We may use your personal information to comply with legal obligations,
-        enforce our terms and conditions, and protect our rights, property, or
-        safety, as required by applicable laws and regulations.
-      </p>
+        <h2>{bodysection2.title}</h2>
+        <p>
+          {bodysection2.subtitle}
+        </p>
+        {bodysection2.content.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
 
-      <h2>3. Data Retention and Security</h2>
-      <p>
-        3.1 Data Retention:
-        <br />
-        We retain your personal information only as long as necessary for the
-        purposes outlined in this Privacy Policy, or as required by law. We will
-        securely delete or anonymize your personal information when it is no
-        longer needed.
-      </p>
+        <p>
+          {bodysection202.subtitle}
+          <br />
+          {bodysection202.content}
+        </p>
 
-      <p>
-        3.2 Data Security:
-        <br />
-        We take appropriate technical and organizational measures to protect
-        your personal information from unauthorized access, loss, misuse,
-        alteration, or disclosure. However, please be aware that no security
-        measures are foolproof, and we cannot guarantee the absolute security of
-        your information.
-      </p>
+        <h2>{bodysection3.title}</h2>
+        <p>
+          {bodysection3.subtitle}
+          <br />
+          {bodysection3.content}
+        </p>
 
-      <h2>4. Disclosure of Information</h2>
-      <p>
-        4.1 Third-Party Service Providers:
-        <br />
-        We may engage trusted third-party service providers to assist us in
-        delivering our services and managing our website. These providers are
-        obligated to maintain the confidentiality and security of your personal
-        information and may only use it in accordance with our instructions.
-      </p>
+        <p>
+          {bodysection302.subtitle}
+          <br />
+          {bodysection302.content}
+        </p>
 
-      <p>
-        4.2 Legal Requirements:
-        <br />
-        We may disclose your personal information if required to do so by law or
-        if we believe that such action is necessary to (a) comply with a legal
-        obligation, (b) protect and defend our rights or property, (c) prevent
-        or investigate possible wrongdoing, or (d) protect the personal safety
-        of users or the public.
-      </p>
+        <h2>{bodysection4.title}</h2>
+        <p>
+          {bodysection4.subtitle}
+          <br />
+          {bodysection4.content}
+        </p>
 
-      <h2>5. Your Rights</h2>
-      <p>
-        You have certain rights regarding the personal information we hold about
-        you. These rights may include:
-      </p>
-      <p>
-        &#8211; The right to access and obtain a copy of your personal
-        information
-        <br />
-        &#8211; The right to rectify any inaccurate or incomplete personal
-        information
-        <br />
-        &#8211; The right to erase your personal information under certain
-        circumstances
-        <br />
-        &#8211; The right to restrict the processing of your personal
-        information
-        <br />
-        &#8211; The right to object to the processing of your personal
-        information
-        <br />
-        &#8211; The right to data portability
-      </p>
+        <p>
+          {bodysection402.subtitle}
+          <br />
+          {bodysection402.content}
+        </p>
 
-      <p>
-        To exercise your rights or make any privacy-related requests, please
-        contact us using the information provided in the &#8220;Contact
-        Us&#8221; section below.
-      </p>
+        <h2> {bodysectio5.title} </h2>
 
-      <h2>6. International Data Transfers</h2>
-      <p>
-        Your personal information may be transferred to and processed in
-        countries outside your jurisdiction, including countries that may have
-        different data protection laws. We will take appropriate measures to
-        ensure that any international transfers comply with applicable data
-        protection laws.
-      </p>
+        {bodysectio5.content .map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
 
-      <h2>7. Contact Us</h2>
-      <p>
-        If you have any questions, concerns, or requests regarding this Privacy
-        Policy or our privacy practices, please contact us:
-        hello@softwareconsultant.org
-      </p>
+        <p>
+          {bodysection501.content}
+        </p>
+
+        <h2>{bodysection6.Title}</h2>
+        <p>
+          {bodysection6.content}
+        </p>
+
+        <h2>{bodysection7.Title}</h2>
+        <p>
+          {bodysection7.content}
+        </p>
       </div>
     </>
   );
