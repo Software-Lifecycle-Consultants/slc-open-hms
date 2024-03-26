b564a6d90d2473de9e60b0de1660e8989b9bc8a8
chithraka99 <chithrakanayana99@gmail.com>
2024-03-26 18:00:17 +0530

    Documented the code

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -17,9 +17,12 @@ import {
 } from "@/data/privacypolicyPage";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+
+/**
+ *  PrivacyPolicyScreen component represents the Privacy & Policy Screen of the website.
+ */
 
 export const metadata: Metadata = {
   title: "HMS Dev | Privacy Policy",
@@ -33,111 +36,122 @@ function PrivacyPolicy() {
       margin="0 auto" /* Center the container */
       padding="60px" /* Add padding */
     >
-      <Typography variant="h2"><b>{header1Details.title}</b></Typography>
-
-      <Box marginTop={7}>
-      <Typography variant="subtitle1">
-        <b>{header1Details.effectiveDate}</b>
+      <Typography variant="h2"> {/* Render the title from header1Details */}
+        <b>{header1Details.title}</b>
       </Typography>
-      <Typography variant="body1">{header1Details.description}</Typography>
+      <Box marginTop={7}>
+        <Typography variant="subtitle1"> {/* Render the subtitle from header1Details */}
+          <b>{header1Details.effectiveDate}</b> {/* Render the effectiveDate from header1Details */}
+        </Typography>
+        <Typography variant="body1">{header1Details.description}</Typography> {/* Render the Description from header1Details */}
       </Box>
-
       <Box marginTop={4}>
-      <Typography variant="h5">{section1.title}</Typography>
+        <Typography variant="h5">{section1.title}</Typography> {/* Render the title from section1 */}
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section1.subtitle}</Typography>
-        <List dense sx={{ marginTop: -1.8 }}>
+        <Typography variant="subtitle1">{section1.subtitle}</Typography> {/* Render the subtitle from section1 */}
+        {/* Render the content from section1 */}
+        <List dense sx={{ marginTop: -1.8 }}> 
           {section1.content.map((item, index) => (
-            <ListItem key={index}sx={{ py: 0 }}>
+            <ListItem key={index} sx={{ py: 0 }}>
               <Typography variant="body1">{item}</Typography>
             </ListItem>
           ))}
         </List>
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section102.subtitle}</Typography>
-        <List dense sx={{ marginTop: -1.8 }}>
+        <Typography variant="subtitle1">{section102.subtitle}</Typography> {/* Render the subtitle from section102 */}
+        {/* Render the content from section102 */}
+        <List dense sx={{ marginTop: -1.8 }}> 
           {section102.content.map((item, index) => (
-            <ListItem key={index}sx={{ py: 0 }}>
+            <ListItem key={index} sx={{ py: 0 }}>
               <Typography variant="body1">{item}</Typography>
             </ListItem>
           ))}
         </List>
       </Box>
-
       <Box marginTop={4}>
-        <Typography variant="h5">{section2.title}</Typography>
+        <Typography variant="h5">{section2.title}</Typography> {/* Render the title from section2 */}
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section2.subtitle}</Typography>
-        <List dense sx={{ marginTop: -1.8 }}>
+        <Typography variant="subtitle1">{section2.subtitle}</Typography> {/* Render the subtitle from section2 */}
+        {/* Render the content from section2*/}
+        <List dense sx={{ marginTop: -1.8 }}> 
           {section2.content.map((item, index) => (
-            <ListItem key={index}sx={{ py: 0 }}>
+            <ListItem key={index} sx={{ py: 0 }}>
               <Typography variant="body1">{item}</Typography>
             </ListItem>
           ))}
         </List>
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section202.subtitle}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section202.content}</Typography>
+        <Typography variant="subtitle1">{section202.subtitle}</Typography> {/* Render the subtitle from section202 */}
+        {/* Render the content from section202 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section202.content}
+        </Typography>
       </Box>
-
       <Box marginTop={4}>
-        <Typography variant="h5">{section3.title}</Typography>
+        <Typography variant="h5">{section3.title}</Typography> {/* Render the title from section3 */}
       </Box>
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section3.subtitle}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section3.content}</Typography>
+        <Typography variant="subtitle1">{section3.subtitle}</Typography> {/* Render the subtitle from section3 */}
+        {/* Render the content from section3 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section3.content}
+        </Typography>
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section302.subtitle}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section302.content}</Typography>
+        <Typography variant="subtitle1">{section302.subtitle}</Typography> {/* Render the subtitle from section302 */}
+        {/* Render the content from section302 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section302.content}
+        </Typography>
       </Box>
-
       <Box marginTop={4}>
-        <Typography variant="h5">{section4.title}</Typography>
+        <Typography variant="h5">{section4.title}</Typography> {/* Render the title from section4 */}
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section4.subtitle}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section4.content}</Typography>
+        <Typography variant="subtitle1">{section4.subtitle}</Typography> {/* Render the subtitle from section4 */}
+        {/* Render the content from section4 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section4.content}
+        </Typography>
       </Box>
-
       <Box marginTop={2}>
-        <Typography variant="subtitle1">{section402.subtitle}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section402.content}</Typography>
+        <Typography variant="subtitle1">{section402.subtitle}</Typography> {/* Render the subtitle from section402 */}
+        {/* Render the content from section402 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section402.content}
+        </Typography>
       </Box>
-
       <Box marginTop={4}>
-        <Typography variant="h5">{sectio5.title}</Typography>
+        <Typography variant="h5">{sectio5.title}</Typography> {/* Render the title from section5 */}
+        {/* Render the content from section5 */}
         <List dense sx={{ marginTop: -1.8 }}>
           {sectio5.content.map((item, index) => (
-            <ListItem key={index}sx={{ py: 0 }}>
+            <ListItem key={index} sx={{ py: 0 }}>
               <Typography variant="body1">{item}</Typography>
             </ListItem>
           ))}
         </List>
       </Box>
-
       <Box marginTop={-1}>
-        <Typography variant="body1">{section501.content}</Typography>
+        <Typography variant="body1">{section501.content}</Typography>{/* Render the content from section501 */}
       </Box>
-
       <Box marginTop={4}>
-        <Typography variant="h5">{section6.Title}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section6.content}</Typography>
+        <Typography variant="h5">{section6.Title}</Typography> {/* Render the title from section6 */}
+        {/* Render the content from section6 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section6.content}
+        </Typography>
       </Box>
-
       <Box marginTop={4}>
-        <Typography variant="h5">{section7.Title}</Typography>
-        <Typography variant="body1" sx={{ marginTop: -1 }}>{section7.content}</Typography>
+        <Typography variant="h5">{section7.Title}</Typography> {/* Render the title from section7 */}
+        {/* Render the content from section7 */}
+        <Typography variant="body1" sx={{ marginTop: -1 }}>
+          {section7.content}
+        </Typography>
       </Box>
     </Box>
   );
