cc32d64b64b9bc61527e5189df2c256a2b03a39f
Sanjeewa Lakmal <87803867+Sanjeewa001@users.noreply.github.com>
2024-04-09 13:54:42 +0530

    Modify component in checkout page as data varify through data files

--- components/checkout/Calender.tsx
+++ components/checkout/Calender.tsx
@@ -4,7 +4,7 @@ import { Box, Typography, Card, CardContent } from "@mui/material";
 import { DateRange,Range} from "react-date-range";
 import "react-date-range/dist/styles.css"; // Import the styles
 import "react-date-range/dist/theme/default.css";
-import { calenderTitle } from "@/data/checkoutPage";
+import { calenderData } from "@/data/checkoutPage";
 import { lora, mulish } from "../../app/fonts";
 
 const Calender = () => {
@@ -44,7 +44,7 @@ const Calender = () => {
               marginTop: "14px",
             }}
           >
-            {calenderTitle}
+            {calenderData.calenderTitle}
           </Typography>
           <Box sx={{ backgroundColor: "" }}>
             {/* Calendar component */}
@@ -69,7 +69,7 @@ const Calender = () => {
               marginTop: "8px",
             }}
           >
-            Terms and Conditions
+            {calenderData.termTitle}
           </Typography>
           {/* Special Request */}
           <Typography
@@ -85,7 +85,7 @@ const Calender = () => {
               marginTop: "16px",
             }}
           >
-            Input your Special Request in Here
+           {calenderData.term1}
           </Typography>
           <Typography
             variant="body1"
@@ -100,7 +100,7 @@ const Calender = () => {
               marginTop: "16px",
             }}
           >
-            Input your Special Request in Here
+            {calenderData.term2}
           </Typography>
           <Typography
             variant="body1"
@@ -115,7 +115,7 @@ const Calender = () => {
               marginTop: "16px",
             }}
           >
-            Input your Special Request in Here
+            {calenderData.term3}
           </Typography>
         </CardContent>
       </Card>
