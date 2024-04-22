a6c6085a9d26675bd858a6a9bd56d3f79cc50308
Sanjeewa Lakmal <87803867+Sanjeewa001@users.noreply.github.com>
2024-04-22 16:34:50 +0530

    Dcomentate the banner content in blog page

--- components/blogPage/Banner1.tsx
+++ components/blogPage/Banner1.tsx
@@ -1,16 +1,17 @@
 import { Typography,Grid,TextField,Button } from "@mui/material";
-
+// banner section of the blog page
 const Banner = () => {
   return(
     <div style={{textAlign:"center"}}>
+      {/* text in the banner  */}
     <Typography sx={{fontSize:"16px"}}>blog</Typography>
     <Typography sx={{fontSize:"48px"}}>Sri Lankan Marvels</Typography>
     <Typography sx={{fontSize:"20px"}}>Celebrate the Beauty of Sri Lanka: Explore, Discover, and Be Enchanted!</Typography>
-    
+    {/* text area to enter the email */}
     <div style={{alignItems:"center", marginTop:"15px",}}>
     <TextField id="filled-basic" label="Enter your email" variant="filled" sx={{backgroundColor:"white",borderRadius:"8px",height:"48px",
         width:"25%", fontSize:"14px",}}/>
-
+    {/* button in the banner section */}
       <Button sx={{
         marginLeft:"15px",
         height:"48px",
@@ -21,7 +22,7 @@ const Banner = () => {
       variant="contained" >
       Subscribe
       </Button>
-      <Typography>We care about your data in our <a href="/privacy-policy">privacy policy</a></Typography>
+      <Typography sx={{marginBottom: "12%"}}>We care about your data in our <a href="/privacy-policy">privacy policy</a></Typography>
     </div>
     </div>
     
