ac978e9afabb147ba971b6318e314998ed34b5dd
Anjalika11 <anjalikadmwsooriya@gmail.com>
2024-04-24 13:03:29 +0530

    Fixed Register page details issues

--- data/registerPage.ts
+++ data/registerPage.ts
@@ -1,5 +1,6 @@
 /* Assign to the Register page Details */
 export const registerPageDetails ={
+    registerPageWebLink: "https://slc-open-hms-api.azurewebsites.net/register",
     registerPageTitle: "Get Started !",
     registerPageSubtitle: "Be a Part of Our Jounrney....",
     registerPageFirstNameSection: "First Name",
@@ -10,7 +11,7 @@ export const registerPageDetails ={
     registerPagePasswordSection: "Password",
     registerPageConfirmPwdSection: "Confirm Password",
     registerPageAgreeRulesSection: "I have agree to the rules," ,
-    registerPageTermsSection: "Terms and conditions.",
+    registerPageTermsandConditionsSection: "Terms and Conditions.",
     registerPageButtonSection: "Sign Up",
     registerPageHaveAccountSection: "Have an account?",
     registerPageLoginSection: "Login Here",
