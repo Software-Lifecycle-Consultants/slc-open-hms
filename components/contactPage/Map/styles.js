712afb41aec4bee5eef5a858cd1697f9127d86b1
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-04-26 13:46:26 +0530

    Created a new file for adjust the map height using media queries but not imported to map.tsx file

---
+++ components/contactPage/Map/styles.js
@@ -0,0 +1,17 @@
+// styles.js
+
+export const mapContainerStyles = {
+  width: "100%",
+
+  "@media (max-width: 767px)": {
+    height: "700px",
+  },
+
+  "@media screen and (min-width: 768px) and (max-width: 1023px)": {
+    height: "700px",
+  },
+
+  "@media screen and (min-width: 1024px)": {
+    height: "400px",
+  },
+};
