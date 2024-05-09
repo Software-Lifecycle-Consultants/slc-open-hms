dd1102f8322240f93fd62834219376b779c707eb
iamarasekera <iamarasekera.dev@gmail.com>
2024-05-09 18:06:21 +0800

    Testing deployment issue in provacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { Metadata } from "next";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+
+
+
+/**
+ *  PrivacyPolicyScreen component represents the Privacy & Policy Screen of the website.
+ */
+
+
+function PrivacyPolicy() {
+
+
+  return (
+    <>
+
+        </>
+  );
+}
+
+export default PrivacyPolicy;
