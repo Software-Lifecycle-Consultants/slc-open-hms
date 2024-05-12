7b85f8e784573409c73572a8aff58ff0ac84ec03
iamarasekera <iamarasekera.dev@gmail.com>
2024-05-12 19:13:21 +0800

    Updated the main components to show footer if required

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,9 +1,13 @@
+// MainContent.tsx
 import { Box, Container } from '@mui/material';
 import Footer from '../components/homePage/Footer';
 
-// This component encapsulates the styling logic for the children component and the footer:
+interface MainContentProps {
+  children: React.ReactNode;
+  showFooter?: boolean; // Optional prop to control footer visibility
+}
 
-const MainContent = ({ children }: { children: React.ReactNode }) => {
+const MainContent = ({ children, showFooter = true }: MainContentProps) => {
   return (
     <Box
       sx={{
@@ -19,18 +23,20 @@ const MainContent = ({ children }: { children: React.ReactNode }) => {
       >
         <div className="container">{children}</div>
       </Box>
-      <Box
-        sx={{
-          flexShrink: 0,
-          backgroundColor: '#1A242D',
-          padding: '20px 0',
-          width: '100%',
-        }}
-      >
-        <Container>
-          <Footer />
-        </Container>
-      </Box>
+      {showFooter && ( // Render footer only if showFooter is true
+        <Box
+          sx={{
+            flexShrink: 0,
+            backgroundColor: '#1A242D',
+            padding: '20px 0',
+            width: '100%',
+          }}
+        >
+          <Container>
+            <Footer />
+          </Container>
+        </Box>
+      )}
     </Box>
   );
 };
