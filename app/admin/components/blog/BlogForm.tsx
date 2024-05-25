8d8e791d8249da2dcc82f4adecb03416c6c89900
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-05-25 20:38:20 +0530

    Remove the border line which covered the blogform and make as it in the UI

--- app/admin/components/blog/BlogForm.tsx
+++ app/admin/components/blog/BlogForm.tsx
@@ -101,341 +101,339 @@ const BlogForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Card sx={{ marginLeft: "auto", marginRight: "auto" }}>
-        <CardContent sx={{ justifyContent: "center", alignItems: "center" }}>
+      <Box sx={{ justifyContent: "center", alignItems: "center" }}>
+        <Typography
+          variant="h2"
+          className={mulish.className}
+          sx={{
+            padding: "10px 20px 10px 20px",
+            fontSize: { xs: "26px", sm: "28px", md: "32px" },
+            fontWeight: 800,
+            color: "#4A5472",
+          }}
+        >
+          Blog
+        </Typography>
+        <Grid
+          container
+          spacing={{ xs: 1, sm: 2, md: 2 }}
+          sx={{
+            display: "block",
+            padding: { xs: "10px 20px 10px 20px" },
+          }}
+        >
+          {/* Title field */}
+          <Grid item>
+            <TextField
+              label="Add title"
+              fullWidth
+              variant="outlined"
+              type="text"
+              size="small"
+              name="title"
+              value={formValues.title}
+              onChange={handleInputChange}
+            />
+          </Grid>
+
+          {/* Sub title field */}
+          <Grid item>
+            <TextField
+              label="Sub title"
+              fullWidth
+              variant="outlined"
+              type="text"
+              size="small"
+              name="subTitle"
+              value={formValues.subTitle}
+              onChange={handleInputChange}
+            />
+          </Grid>
+
+          {/* Tags field */}
+          <Grid item>
+            <Autocomplete
+              clearIcon={false}
+              options={[]}
+              freeSolo
+              multiple
+              renderTags={(value, getTagProps) =>
+                value.map((option, index) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <Chip label={option} {...getTagProps({ index })} />
+                ))
+              }
+              renderInput={(params) => (
+                <TextField label="Add Tags" {...params} size="small" />
+              )}
+              onChange={handleTagsChange}
+              value={formValues.tags}
+            />
+          </Grid>
+
+          {/* TextEditor component for rich text editing */}
+          <Grid item>
+            <TextEditor
+              onBodyChange={handleBodyChange}
+              bodyContent={formValues.bodyContent}
+            />
+          </Grid>
+
           <Typography
-            variant="h2"
+            variant="h3"
             className={mulish.className}
             sx={{
               padding: "10px 20px 10px 20px",
-              fontSize: { xs: "26px", sm: "28px", md: "32px" },
-              fontWeight: 800,
+              fontSize: "16px",
+              fontWeight: 500,
               color: "#4A5472",
             }}
           >
-            Blog
+            Cover Image
           </Typography>
+        </Grid>
+        <Grid
+          container
+          spacing={{ xs: 1, sm: 2, md: 2 }}
+          sx={{
+            display: "block",
+            padding: { xs: "10px 20px 10px 20px" },
+            marginBottom: "20px",
+          }}
+        >
+          {/* Cover Image Upload */}
           <Grid
-            container
-            spacing={{ xs: 1, sm: 2, md: 2 }}
-            sx={{
-              display: "block",
-              padding: { xs: "10px 20px 10px 20px" },
-            }}
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            sx={{ alignItems: "center" }}
           >
-            {/* Title field */}
-            <Grid item>
-              <TextField
-                label="Add title"
-                fullWidth
-                variant="outlined"
-                type="text"
-                size="small"
-                name="title"
-                value={formValues.title}
-                onChange={handleInputChange}
-              />
-            </Grid>
-
-            {/* Sub title field */}
-            <Grid item>
-              <TextField
-                label="Sub title"
-                fullWidth
-                variant="outlined"
-                type="text"
-                size="small"
-                name="subTitle"
-                value={formValues.subTitle}
-                onChange={handleInputChange}
-              />
-            </Grid>
-
-            {/* Tags field */}
-            <Grid item>
-              <Autocomplete
-                clearIcon={false}
-                options={[]}
-                freeSolo
-                multiple
-                renderTags={(value, getTagProps) =>
-                  value.map((option, index) => (
-                    // eslint-disable-next-line react/jsx-key
-                    <Chip label={option} {...getTagProps({ index })} />
-                  ))
-                }
-                renderInput={(params) => (
-                  <TextField label="Add Tags" {...params} size="small" />
-                )}
-                onChange={handleTagsChange}
-                value={formValues.tags}
-              />
-            </Grid>
-
-            {/* TextEditor component for rich text editing */}
-            <Grid item>
-              <TextEditor
-                onBodyChange={handleBodyChange}
-                bodyContent={formValues.bodyContent}
-              />
-            </Grid>
-
-            <Typography
-              variant="h3"
-              className={mulish.className}
+            <Card
+              elevation={0}
               sx={{
-                padding: "10px 20px 10px 20px",
-                fontSize: "16px",
-                fontWeight: 500,
-                color: "#4A5472",
-              }}
-            >
-              Cover Image
-            </Typography>
-          </Grid>
-          <Grid
-            container
-            spacing={{ xs: 1, sm: 2, md: 2 }}
-            sx={{
-              display: "block",
-              padding: { xs: "10px 20px 10px 20px" },
-              marginBottom: "20px",
-            }}
-          >
-            {/* Cover Image Upload */}
-            <Grid
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-              sx={{ alignItems: "center" }}
-            >
-              <Card
-                elevation={0}
-                sx={{
-                  height: {
-                    xs: "300px",
-                    sm: "450px",
-                    md: "450px",
-                    lg: "500px",
-                    xl: "550px",
-                  },
-                  width: "auto",
-
-                  border: "1px solid",
-                  borderRadius: "8px",
-                  borderColor: "#D0D5DD",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding: "16px",
-                }}
-              >
-                <Button
-                  sx={{
-                    marginTop: "7px",
-                    backgroundColor: "#4A5472",
-                  }}
-                  component="label"
-                  role={undefined}
-                  variant="contained"
-                  tabIndex={-1}
-                  startIcon={<AddAPhotoIcon />}
-                >
-                  <VisuallyHiddenInput type="file" />
-                </Button>
-              </Card>
-            </Grid>
-
-            <Typography
-              variant="h3"
-              className={mulish.className}
-              sx={{
-                padding: "10px 20px 10px 20px",
-                fontSize: "16px",
-                fontWeight: 500,
-                color: "#4A5472",
-              }}
-            >
-              Author Image
-            </Typography>
-
-            {/* Author Image Upload */}
-            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <Card
-                elevation={0}
-                sx={{
-                  height: {
-                    xs: "300px",
-                    sm: "450px",
-                    md: "450px",
-                    lg: "500px",
-                    xl: "550px",
-                  },
-                  width: "auto",
-                  border: "1px solid",
-                  borderRadius: "8px",
-                  borderColor: "#D0D5DD",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding: "16px",
-                }}
-              >
-                <Button
-                  sx={{
-                    marginTop: "7px",
-                    backgroundColor: "#4A5472",
-                  }}
-                  component="label"
-                  role={undefined}
-                  variant="contained"
-                  tabIndex={-1}
-                  startIcon={<CloudUploadIcon />}
-                >
-                  <VisuallyHiddenInput type="file" />
-                </Button>
-              </Card>
-            </Grid>
-
-            {/* Author field */}
-            <Grid item>
-              <TextField
-                label="Author"
-                fullWidth
-                variant="outlined"
-                type="text"
-                size="small"
-                name="author"
-                value={formValues.author}
-                onChange={handleInputChange}
-              />
-            </Grid>
-
-            {/* Author Description field */}
-            <Grid item>
-              <TextField
-                label="Author Description..."
-                fullWidth
-                multiline
-                rows={6}
-                variant="outlined"
-                type="message"
-                name="authorDescription"
-                value={formValues.authorDescription}
-                onChange={handleInputChange}
-              />
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            spacing={{ xs: 1, sm: 2, md: 2 }}
-            sx={{
-              display: "block",
-              padding: { xs: "10px 20px 10px 20px" },
-              marginBottom: "20px",
-            }}
-          >
-            <Typography
-              variant="h3"
-              className={mulish.className}
-              sx={{
-                padding: "10px 20px 10px 20px",
-                fontSize: "16px",
-                fontWeight: 500,
-                color: "#4A5472",
-              }}
-            >
-              Social Media Links
-            </Typography>
-
-            {/* Social Media Links fields */}
-            <Grid item>
-              <TextField
-                label="Twitter"
-                fullWidth
-                variant="outlined"
-                type="text"
-                size="small"
-                name="twitter"
-                value={formValues.twitter}
-                onChange={handleInputChange}
-              />
-            </Grid>
-
-            <Grid item>
-              <TextField
-                label="Facebook"
-                fullWidth
-                variant="outlined"
-                type="text"
-                size="small"
-                name="facebook"
-                value={formValues.facebook}
-                onChange={handleInputChange}
-              />
-            </Grid>
-
-            <Grid item>
-              <TextField
-                label="LinkedIn"
-                fullWidth
-                variant="outlined"
-                type="text"
-                size="small"
-                name="linkedin"
-                value={formValues.linkedin}
-                onChange={handleInputChange}
-              />
-            </Grid>
-          </Grid>
-
-          <Box
-            sx={{
-              textAlign: "right",
-              padding: "0 40px 30px",
-              //display: { xs: "flex" },
-            }}
-          >
-            <Button
-              type="button"
-              sx={{
-                backgroundColor: "#FFFFFF",
-                border: "2px solid black",
-                borderRadius: "10px",
-                color: "black",
-                padding: { xs: "8px 14px" },
-                marginRight: "10px",
-              }}
-              onClick={() => console.log("Draft Saved")}
-            >
-              Save Draft
-            </Button>
-
-            <Button
-              type="submit"
-              sx={{
-                backgroundColor: "#4A5472",
-                borderRadius: "10px",
-                color: "white",
-                border: "2px solid ",
-                padding: { xs: "8px 14px" },
-                "&:hover": {
-                  backgroundColor: "#3B435F",
-                  borderColor: "#3B435F",
+                height: {
+                  xs: "300px",
+                  sm: "450px",
+                  md: "450px",
+                  lg: "500px",
+                  xl: "550px",
                 },
-                "&:active": {
-                  backgroundColor: "#2C3045",
-                  borderColor: "#2C3045",
-                },
+                width: "auto",
+
+                border: "1px solid",
+                borderRadius: "8px",
+                borderColor: "#D0D5DD",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "16px",
               }}
             >
-              Publish
-            </Button>
-          </Box>
-        </CardContent>
-      </Card>
+              <Button
+                sx={{
+                  marginTop: "7px",
+                  backgroundColor: "#4A5472",
+                }}
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<AddAPhotoIcon />}
+              >
+                <VisuallyHiddenInput type="file" />
+              </Button>
+            </Card>
+          </Grid>
+
+          <Typography
+            variant="h3"
+            className={mulish.className}
+            sx={{
+              padding: "10px 20px 10px 20px",
+              fontSize: "16px",
+              fontWeight: 500,
+              color: "#4A5472",
+            }}
+          >
+            Author Image
+          </Typography>
+
+          {/* Author Image Upload */}
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Card
+              elevation={0}
+              sx={{
+                height: {
+                  xs: "300px",
+                  sm: "450px",
+                  md: "450px",
+                  lg: "500px",
+                  xl: "550px",
+                },
+                width: "auto",
+                border: "1px solid",
+                borderRadius: "8px",
+                borderColor: "#D0D5DD",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "16px",
+              }}
+            >
+              <Button
+                sx={{
+                  marginTop: "7px",
+                  backgroundColor: "#4A5472",
+                }}
+                component="label"
+                role={undefined}
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<CloudUploadIcon />}
+              >
+                <VisuallyHiddenInput type="file" />
+              </Button>
+            </Card>
+          </Grid>
+
+          {/* Author field */}
+          <Grid item>
+            <TextField
+              label="Author"
+              fullWidth
+              variant="outlined"
+              type="text"
+              size="small"
+              name="author"
+              value={formValues.author}
+              onChange={handleInputChange}
+            />
+          </Grid>
+
+          {/* Author Description field */}
+          <Grid item>
+            <TextField
+              label="Author Description..."
+              fullWidth
+              multiline
+              rows={6}
+              variant="outlined"
+              type="message"
+              name="authorDescription"
+              value={formValues.authorDescription}
+              onChange={handleInputChange}
+            />
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={{ xs: 1, sm: 2, md: 2 }}
+          sx={{
+            display: "block",
+            padding: { xs: "10px 20px 10px 20px" },
+            marginBottom: "20px",
+          }}
+        >
+          <Typography
+            variant="h3"
+            className={mulish.className}
+            sx={{
+              padding: "10px 20px 10px 20px",
+              fontSize: "16px",
+              fontWeight: 500,
+              color: "#4A5472",
+            }}
+          >
+            Social Media Links
+          </Typography>
+
+          {/* Social Media Links fields */}
+          <Grid item>
+            <TextField
+              label="Twitter"
+              fullWidth
+              variant="outlined"
+              type="text"
+              size="small"
+              name="twitter"
+              value={formValues.twitter}
+              onChange={handleInputChange}
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              label="Facebook"
+              fullWidth
+              variant="outlined"
+              type="text"
+              size="small"
+              name="facebook"
+              value={formValues.facebook}
+              onChange={handleInputChange}
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              label="LinkedIn"
+              fullWidth
+              variant="outlined"
+              type="text"
+              size="small"
+              name="linkedin"
+              value={formValues.linkedin}
+              onChange={handleInputChange}
+            />
+          </Grid>
+        </Grid>
+
+        <Box
+          sx={{
+            textAlign: "right",
+            padding: "0 40px 30px",
+            //display: { xs: "flex" },
+          }}
+        >
+          <Button
+            type="button"
+            sx={{
+              backgroundColor: "#FFFFFF",
+              border: "2px solid black",
+              borderRadius: "10px",
+              color: "black",
+              padding: { xs: "8px 14px" },
+              marginRight: "10px",
+            }}
+            onClick={() => console.log("Draft Saved")}
+          >
+            Save Draft
+          </Button>
+
+          <Button
+            type="submit"
+            sx={{
+              backgroundColor: "#4A5472",
+              borderRadius: "10px",
+              color: "white",
+              border: "2px solid ",
+              padding: { xs: "8px 14px" },
+              "&:hover": {
+                backgroundColor: "#3B435F",
+                borderColor: "#3B435F",
+              },
+              "&:active": {
+                backgroundColor: "#2C3045",
+                borderColor: "#2C3045",
+              },
+            }}
+          >
+            Publish
+          </Button>
+        </Box>
+      </Box>
     </form>
   );
 };
