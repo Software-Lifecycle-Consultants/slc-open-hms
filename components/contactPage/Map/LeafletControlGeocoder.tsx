e3c2660f6974a72fcffbef0284fa2b205c59cd12
sinera2000 <wijethungawms.20@itfac.mrt.ac.lk>
2024-05-27 10:02:52 +0530

    Documented the file

--- components/contactPage/Map/LeafletControlGeocoder.tsx
+++ components/contactPage/Map/LeafletControlGeocoder.tsx
@@ -6,13 +6,26 @@ import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import L from "leaflet";
 import { geocoders } from "leaflet-control-geocoder";
 
+/**
+ * Interface defining the structure for position information.
+ */
+
 interface PositionInfo {
   address: string;
 }
 
+/**
+ * Interface defining the props for the LeafletControlGeocoder component.
+ */
+
 interface LeafletControlGeocoderProps {
   positionInfos: PositionInfo[];
 }
+
+/**
+ * LeafletControlGeocoder component that integrates the Leaflet Control Geocoder to convert addresses to latitude and longitude.
+ * It adds markers to the map for each address provided.
+ */
 
 const LeafletControlGeocoder: React.FC<LeafletControlGeocoderProps> = (
   props
@@ -21,12 +34,17 @@ const LeafletControlGeocoder: React.FC<LeafletControlGeocoderProps> = (
   const { positionInfos } = props;
 
   useEffect(() => {
+    // Ensure the map instance is available
     if (!map) {
       console.error("Map instance is not available");
       return;
     }
 
     const geocoder = new geocoders.Nominatim();
+
+    /**
+     * Function to add markers for each address provided.
+     */
 
     const addMarkers = async () => {
       for (const positionInfo of positionInfos) {
@@ -38,6 +56,7 @@ const LeafletControlGeocoder: React.FC<LeafletControlGeocoderProps> = (
               const result = resultArray[0];
               const latlng = result.center;
 
+              //Custom marker
               const customIcon = new L.Icon({
                 iconUrl: "https://www.svgrepo.com/show/362123/map-marker.svg",
                 iconSize: [30, 50],
@@ -71,8 +90,12 @@ const LeafletControlGeocoder: React.FC<LeafletControlGeocoderProps> = (
   return null;
 };
 
+//Function to geocode an address using the specified geocoder.
+
 const geocodeAddress = (geocoder: any, address: string): Promise<any[]> => {
+  //A promise that resolves to an array of geocoding results.
   return new Promise((resolve, reject) => {
+    //The geocoder to use for the address conversion to geocode.
     geocoder.geocode(address, (results: any[]) => {
       if (results) {
         resolve(results);
