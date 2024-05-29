ac7f1b7e43917d427d56febf6c05fcad0124ffaf
chithraka99 <chithrakanayana99@gmail.com>
2024-05-29 09:55:26 +0530

    Created the Admin setting Data File

---
+++ data/adminSetting.ts
@@ -0,0 +1,11 @@
+//Asign details to Admin Setting Page
+export const adminSettingData = {
+    adminSettingTitle01: "Admin",
+    adminSettingEmail:"admin.01@gmail.com",
+    adminSettingCustomerSupportButton:"Customer Support",
+    adminSettingTitle02: "Update Credentials",
+    adminSettingTitle03: "Change E mail",
+    adminSettingTitle04: "Change Password",
+    adminSettingSaveButton:"Save",
+
+}
