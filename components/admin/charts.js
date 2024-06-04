dc298dde631e4c11a506a905d1eb245ab6f7c135
Kasun Rasanjana <141154279+Kasunkule@users.noreply.github.com>
2024-06-04 13:27:00 +0530

    Update the margin around the 2 graphs and Leave a space between the 2 graphs fixed

--- components/admin/charts.js
+++ components/admin/charts.js
@@ -1,18 +1,23 @@
-'use client'
+'use client';
+import { Box } from "@mui/material";
 import AreaChartPlot from "./AreaChartPlot";
 import BarChartPlot from "./BarChartPlot";
 
 const Charts = () => {
   return (
     <>
-
-      <section className="flex my-4 px-4 gap-3">
-        <AreaChartPlot/>
-      </section>
-
-      <section className="flex my-4 px-4 gap-3">
-        <BarChartPlot/>
-      </section>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 6,padding: '80px' }}> {/* Outer Box */}
+        <Box sx={{ backgroundColor: 'white', padding: '50px',borderRadius:5 }}> {/* White Box 1 */}
+          <section className="flex my-4 px-4 gap-3">
+            <AreaChartPlot />
+          </section>
+        </Box>
+        <Box sx={{ backgroundColor: 'white', padding: '50px',borderRadius:5 }}> {/* White Box 2 */}
+          <section className="flex my-4 px-4 gap-3">
+            <BarChartPlot />
+          </section>
+        </Box>
+      </Box>
     </>
   );
 };
