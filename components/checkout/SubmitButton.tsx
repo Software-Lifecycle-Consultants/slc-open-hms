7422fcbf523fa80ff38c4395ea6a463a13116241
Sanjeewa Lakmal <87803867+Sanjeewa001@users.noreply.github.com>
2024-06-04 23:22:43 +0530

    Reworked in checkbox and submit button in checkout page

--- components/checkout/SubmitButton.tsx
+++ components/checkout/SubmitButton.tsx
@@ -1,16 +1,36 @@
+// This line tells Next.js that this component should be rendered on the client-side
+"use client";
+
+// Import required components and hooks from the respective libraries
 import { Button, Checkbox, Typography, Box, Link } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { submitContent } from "@/data/checkoutPage";
 import { mulish } from "../../app/fonts";
 import { orangebuttonCommonStyle } from "../homePage/styles";
 import PDFGenerator from "../pdf-generator/page";
+
+// Define an object with props for the Checkbox component
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
+// Define the SubmitButton component
 const SubmitButton = () => {
+  // Use the useState hook to manage the state of the checkbox
+  const [isChecked, setIsChecked] = useState(false);
+
+  // Define a function to handle the checkbox change event
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  // Render the component
   return (
     <>
+      {/* Display a flex container with the checkbox and condition text */}
       <Box sx={{ display: "flex" }}>
+        {/* Render the Checkbox component */}
         <Checkbox
+          checked={isChecked}
+          onChange={handleCheckboxChange}
           {...label}
           sx={{
             "& .MuiSvgIcon-root": { fontSize: 34 },
@@ -21,6 +41,7 @@ const SubmitButton = () => {
             marginTop: "8px",
           }}
         />
+        {/* Render the condition text */}
         <Typography
           variant="body1"
           className={mulish.className}
@@ -32,15 +53,21 @@ const SubmitButton = () => {
             letterSpacing: "0.08px",
             fontWeight: "600",
             marginTop: "24px",
-          }}>
+          }}
+        >
           {submitContent.condition}
         </Typography>
       </Box>
+      {/* Render the Submit button */}
       <Link href={"/pdf-genarator"}>
       <Button
         variant="contained"
+        disabled={!isChecked} // Disable the button if the checkbox is not checked
         className={mulish.className}
-        sx={{...orangebuttonCommonStyle ,marginBottom: "100px" }}
+        sx={{
+          ...orangebuttonCommonStyle,
+          marginBottom: "100px",
+        }}
       >
         {submitContent.submitctaCaption}
       </Button>
@@ -49,4 +76,5 @@ const SubmitButton = () => {
   );
 };
 
+// Export the SubmitButton component
 export default SubmitButton;
