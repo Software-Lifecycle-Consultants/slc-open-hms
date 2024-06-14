00e75932b1487412da746c8bc89f6b5d44af3c43
iamarasekera <iamarasekera.dev@gmail.com>
2024-06-14 16:30:43 +0800

    Removed unnecessay imports

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,13 +1,7 @@
 "use client"
-import Calender from "@/components/frontend/checkoutPage/Calender";
-import Heading from "@/components/frontend/checkoutPage/Heading";
-import SubmitButton from "@/components/frontend/checkoutPage/SubmitButton";
-import { Grid, Box, Container } from "@mui/material";
+import {Box} from "@mui/material";
 import React from "react";
-import BillingDetails from "@/components/frontend/checkoutPage/BillingDetails";
-import BookingInfo from "@/components/frontend/checkoutPage/BookingInfo";
-import ExtraFacilitation from "@/components/frontend/checkoutPage/ExtraFacilitation";
-import Charts from "@/components/admin/dashboard/charts";
+import Charts from "@/components/admin/dashboard/Charts";
 /**
  * CheckoutScreen component represents the checkout screen of the website.
  * It includes sections like Heading, Billing details form, Booking info, Extra facilitation, Calender and Submit button.
