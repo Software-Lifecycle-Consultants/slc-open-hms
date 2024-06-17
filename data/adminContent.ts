102dbcb783f42639ea95d55d02ff28e3a02bb0f9
paranga <paranga@gmail.com>
2024-06-17 13:26:44 +0530

    Created Data-adminContent.ts

---
+++ data/adminContent.ts
@@ -0,0 +1,43 @@
+// Compare this snippet from app/admin/components/content/page.tsx:
+export const adminContentPage = {
+    adminContentPageHeaderTitle: "Edit Content",
+    adminContentPageSubmitButton:"Edit"
+}
+
+// Compare this snippet from app/admin/components/content/HeroBanner.tsx:
+export const adminContentHeroBanner = {
+    adminContentHeaderTitle: "Hero Banner",
+    adminContentBannerDetailsTitle01:"Banner Title",
+    adminContentBannerDetailsTitle02:"Banner Sub Title",   
+    adminContentBannerDetailsTitle03:"Banner Description",
+    adminContentBannerDetailsTitle04:"Banner Button 1",
+    adminContentBannerDetailsTitle05:"Banner Button 2",
+    adminContentBannerDetailsEditButton:"Edit",
+    adminContentBannerDetailsSubmitButton:"Save"
+}
+// Compare this snippet from app/admin/components/content/DestinationOverview.tsx:
+export const adminContentDestinationOverview = {
+    adminContentMainBarHeaderTitle: "Destination Overview",
+    adminContentMainBarTitle01:"Main Banner Title",
+    adminContentMainBarTitle02:"Main Banner Description",   
+    adminContentMainBarTitle03:"Main Banner Image",
+    adminContentMainBarUploadButton:"Upload file",
+    adminContentMainBarCard01:"Card 01",
+     adminContentCardMainBarTitle01:"Card 01 Title",
+     adminContentCardMainBarDesTitle01:"Card 01  Description",
+    adminContentMainBarCard02:"Card 02",
+     adminContentCardMainBarTitle02:"Card 02 Title",
+     adminContentCardMainBarDesTitle02:"Card 02  Description",
+    adminContentMainBarCard03:"Card 03",
+     adminContentCardMainBarTitle03:"Card 03 Title",
+     adminContentCardMainBarDesTitle03:"Card 03  Description",
+    adminContentMainBarSubmitButton:"Save",
+    adminContentMainBarEditButton:"Edit"
+}
+
+// Compare this snippet from app/admin/components/content/LogoCarousel.tsx:
+export const adminContentLogoCarousel = {
+    adminContentLogoCarouselHeaderTitle: "Logo Carousel",
+    adminContentLogoCarouselUploadButton:"Upload file",
+    adminContentLogoCarouselSubmitButton:"Submit"
+}
