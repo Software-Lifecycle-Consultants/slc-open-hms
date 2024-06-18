1f8cca760cb64824a9cd29b8e4a6c235501fe182
iamarasekera <iamarasekera.dev@gmail.com>
2024-06-18 14:46:16 +0800

    Resolved conflicts in page tsx related to adding components andd added datafile for admin content

--- app/admin/content/page.tsx
+++ app/admin/content/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import { Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Grid, Button, Typography, Box } from '@mui/material';
+
 import BannerDetails from "@/components/admin/contentPage/BannerDetails";
 import MainBannerCard from "@/components/admin/contentPage/MainBannerCard";
 import BannerCard from "@/components/admin/contentPage/BannerCard";
-=======
-import { Container, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Grid, Button, Typography, Box } from '@mui/material';
-import HeroBanner from "@/app/admin/components/content/HeroBanner";
-import DestinationOverview from "@/app/admin/components/content/DestinationOverview";
-import BannerCard from "@/app/admin/components/content/BannerCard";
-import LogoCarousel from "@/app/admin/components/content/LogoCarousel";
 import { adminContentPage } from "@/data/admincontent";
->>>>>>> feature-admin-content-dup
+
 
 const HomePage: React.FC = () => {
   const [component, setComponent] = useState<string>('');
@@ -53,10 +47,10 @@ const HomePage: React.FC = () => {
               <MenuItem value="a">Logo Carousel</MenuItem>
             </Select>
           </FormControl>
-          {component === "x" && <HeroBanner />}
+          {/* {component === "x" && <HeroBanner />}
           {component === "y" && <DestinationOverview />}
           {component === "z" && <BannerCard />}
-          {component === "a" && <LogoCarousel />}
+          {component === "a" && <LogoCarousel />} */}
         </Container>
       </Grid>
     </Grid>
