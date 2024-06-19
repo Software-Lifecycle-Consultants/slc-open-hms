c357de6adf0cfebb43dae5ff49735987bc60b713
paranga <paranga@gmail.com>
2024-06-19 10:22:24 +0530

    Updated Destination Card component  and for Admin Content data file

--- components/admin/contentPage/DestinationCards.tsx
+++ components/admin/contentPage/DestinationCards.tsx
@@ -3,7 +3,7 @@ import { TextField, Grid, Card, Typography, Box, Button } from "@mui/material";
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-
+import { adminContentDestinationCard } from '@/data/admincontent';
 
 const DestinationCards: React.FC = () => {
   return (
@@ -25,20 +25,25 @@ const DestinationCards: React.FC = () => {
         >
           {/* Display the header title */}
           <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-            Videos
+            {adminContentDestinationCard.adminContentDestinationCardHeaderTitle // Display the header title
+            }
           </Typography>
           <Box
             sx={{
               display: "flex",
-              justifyContent: "space-between",
+              justifyContent: "end",
               height: 30,
               borderColor: "#000",
               borderRadius: "8px",
               border: "1px solid #000",
             }}
           >
-            <Typography ml={2}> Cappadocia</Typography>
-            <IconButton color="secondary" aria-label="edit">
+            <Typography sx={{ flexGrow: 0.9 }}>
+              {
+                adminContentDestinationCard.adminContentDestinationCardButtonCard1 // Display the button card 1
+              }
+            </Typography>
+            <IconButton aria-label="edit">
               <EditIcon />
             </IconButton>
             <IconButton aria-label="delete">
@@ -48,7 +53,7 @@ const DestinationCards: React.FC = () => {
           <Box
             sx={{
               display: "flex",
-              justifyContent: "space-between",
+              justifyContent: "end",
               height: 30,
               borderColor: "#000",
               borderRadius: "8px",
@@ -56,26 +61,46 @@ const DestinationCards: React.FC = () => {
               mt: 2,
             }}
           >
-            <Typography ml={2}> Cappadocia</Typography>
-            <Button endIcon={<EditIcon />} />
+            <Typography sx={{ flexGrow: 0.9 }}>
+              <Typography sx={{ flexGrow: 0.9 }}>
+                {" "}
+                {
+                  adminContentDestinationCard.adminContentDestinationCardButtonCard2 // Display the button card 2
+                }
+              </Typography>
+            </Typography>
+            <IconButton aria-label="edit">
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
           </Box>
           <Box
             sx={{
               display: "flex",
-              justifyContent: "space-between",
               height: 30,
               borderColor: "#000",
               borderRadius: "8px",
               border: "1px solid #000",
               mt: 2,
+              justifyContent: "end",
             }}
           >
-            <Typography ml={2}> Cappadocia</Typography>
-            <Button endIcon={<EditIcon />} />
+            <Typography sx={{ flexGrow: 0.9 }}>
+              {
+                adminContentDestinationCard.adminContentDestinationCardButtonCard3 // Display the button card 3
+              }
+            </Typography>
+            <IconButton aria-label="edit">
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
           </Box>
         </Card>
       </Grid>
-
       {/* Display the New Videos cards  */}
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <Card
@@ -88,30 +113,135 @@ const DestinationCards: React.FC = () => {
             borderColor: "#000",
             flexShrink: "0",
             marginTop: "20px",
-            padding: "20px",
+            padding: "25px",
           }}
         >
           <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-            Add New Videos
+            {
+              adminContentDestinationCard.adminContentDestinationCardHeaderTitle1 // Display the header title
+            }
           </Typography>
           <Typography sx={{ marginTop: "10px" }}>
-            Video Title</Typography>
+            {adminContentDestinationCard.adminContentDestinationCardTitle // Display the video title
+            }
+          </Typography>
           <TextField
             fullWidth
             label="Enter Video  Title"
             variant="outlined"
             required
+            sx={{ m: 1 }}
           />
           <Typography sx={{ marginTop: "10px" }}>
-            Video Description </Typography>
+            {
+              adminContentDestinationCard.adminContentDestinationCardDescription2 // Display the video description
+            }{" "}
+          </Typography>
           <TextField
             fullWidth
             label="Enter Description"
             variant="outlined"
             multiline
             rows={4}
+            sx={{ m: 1 }}
+          />
+          <Typography sx={{ marginTop: "10px" }}>
+            {adminContentDestinationCard.adminContentDestinationCardTitle3 // Display the video URL 1
+            }
+          </Typography>
+          <TextField
+            fullWidth
+            label="Enter Video URL 1 Link"
+            variant="outlined"
+            required
+            sx={{ m: 1 }}
+          />
+          <Typography sx={{ marginTop: "10px" }}> 
+            {adminContentDestinationCard.adminContentDestinationCardTitle4 // Display the video title
+             }
+          </Typography>
+          <TextField
+            fullWidth
+            label="Enter City Name"
+            variant="outlined"
+            required
+            sx={{ m: 1 }}
+          />
+          <Typography sx={{ marginTop: "10px" }}>
+            {" "}
+            {
+              adminContentDestinationCard.adminContentDestinationCardDescription2 // Display the video description
+            }
+          </Typography>
+          <TextField
+            fullWidth
+            label="Enter Description"
+            variant="outlined"
+            multiline
+            rows={4}
+            sx={{ m: 1 }}
+          />
+          <Typography sx={{ marginTop: "10px" }}>
+            {" "}
+            {adminContentDestinationCard.adminContentDestinationCardTitle4 // Display the video title
+            }
+          </Typography>
+          <TextField
+            fullWidth
+            label="Price"
+            variant="outlined"
+            required
+            sx={{ m: 1 }}
+          />
+          <Typography sx={{ marginTop: "10px" }}>
+            {adminContentDestinationCard.adminContentDestinationCardTitle5 // Display the custom tag
+            }
+          </Typography>
+          <TextField
+            fullWidth
+            label="Enter Custom Tag"
+            variant="outlined"
+            required
+            sx={{ m: 1 }}
+          />
+          <Typography sx={{ marginTop: "10px" }}>
+            {adminContentDestinationCard.adminContentDestinationCardTitle6 // Display the button name
+            }
+          </Typography>
+          <TextField
+            fullWidth
+            label="Enter Button Name"
+            variant="outlined"
+            required
+            sx={{ m: 1 }}
           />
         </Card>
+        <Box display="flex" justifyContent="end" alignItems="center">
+          <Button
+            sx={{
+              textTransform: "none",
+              marginTop: "7px",
+              backgroundColor: "#4A5472",
+              "&:hover": { backgroundColor: "#192959" },
+              color: "white",
+              width: "100px",
+              height: "30px",
+              borderRadius: "8px",
+              marginLeft: "10px",
+              textAlign: "center",
+            }}
+            variant="contained"
+          >
+            <Typography
+              style={{
+                color: "white",
+              }}
+            >
+             {adminContentDestinationCard.adminContentDestinationCardSubmitButton // Display the submit button text
+             }
+            </Typography>
+          </Button>
+        </Box>
       </Grid>
     </Grid>
   );
