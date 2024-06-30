02b33e959a0f872ebf9c7e714b2028802ff30833
Pubuduni Mahagedara <chamodya.mahagedara@gmail.com>
2024-06-30 11:16:50 +0530

    Reworked in theme file double quotations to single quotations

--- styles/Theme.tsx
+++ styles/Theme.tsx
@@ -8,77 +8,77 @@ import color from 'material-ui-colors/dist/blue';
 const themeClient = createTheme({
   // Define the color palette for the theme
   palette: {
-    mode: "light", // Set the mode to light
+    mode: 'light', // Set the mode to light
     primary: {
-      main: "#C7923E", // Set the primary color
+      main: '#C7923E', // Set the primary color
     },
     secondary: {
-      main: "#FFFFFF", // Set the secondary color
+      main: '#FFFFFF', // Set the secondary color
     },
     error: {
-      main: "#E83B3B", // Set the error color
+      main: '#E83B3B', // Set the error color
     },
     success: {
-      main: "#52AA44", // Set the success color
+      main: '#52AA44', // Set the success color
     },
     background: {
-      default: "#FFFFFF", // Set the default background color
+      default: '#FFFFFF', // Set the default background color
     },
     text: {
-      primary: "#353535", // Set the primary text color
-      secondary: "#FFFFFF", // Set the secondary text color
+      primary: '#353535', // Set the primary text color
+      secondary: '#FFFFFF', // Set the secondary text color
     },
   },
   // Define typography settings for the theme
   typography: {
-    fontFamily: "Lora, sans-serif", // Set the default font family
+    fontFamily: 'Lora, sans-serif', // Set the default font family
     h1: {
       // Define the h1 typography settings
-      fontSize: "5rem",
+      fontSize: '5rem',
       fontWeight: 700,
-      fontFamily: "Lora",
-      color: "#0C111F",
-      fontstyle: "normal",
-      letterSpacing: "-0.03rem",
-      "@media (max-width:600px)": {
-        fontSize: "3.125rem",
+      fontFamily: 'Lora',
+      color: '#0C111F',
+      fontstyle: 'normal',
+      letterSpacing: '-0.03rem',
+      '@media (max-width:600px)': {
+        fontSize: '3.125rem',
       },
     },
     h2: {
       // Define the h2 typography settings
-      fontSize: "3rem",
+      fontSize: '3rem',
       fontWeight: 700,
-      fontFamily: "Lora",
-      color: "#0C111F",
-      "@media (max-width:600px)": {
-        fontSize: "2.5rem",
-        paddingTop: "1.25rem",
+      fontFamily: 'Lora',
+      color: '#0C111F',
+      '@media (max-width:600px)': {
+        fontSize: '2.5rem',
+        paddingTop: '1.25rem',
       },
     },
     h3: {
       // Define the h3 typography settings
-      fontFamily: "Mulish",
-      fontSize: "1.125rem",
+      fontFamily: 'Mulish',
+      fontSize: '1.125rem',
       fontWeight: 200,
-      letterSpacing: "0.02rem",
-      color: "#353535",
-      lineHeight: "1.75rem",
-      fontStyle: "normal",
-      "@media (max-width:600px)": {
-        fontSize: "1.1rem",
+      letterSpacing: '0.02rem',
+      color: '#353535',
+      lineHeight: '1.75rem',
+      fontStyle: 'normal',
+      '@media (max-width:600px)': {
+        fontSize: '1.1rem',
       },
     },
     h4: {
       // Define the h4 typography settings
-      fontSize: "1rem",
-      lineHeight: "2rem",
-      fontFamily: "Mulish",
-      color: "#FFFFFF",
-      letterSpacing: "-0.01rem",
-      fontStyle: "normal",
-      fontWeight: "200",
-      "@media (max-width:600px)": {
-        fontSize: "1rem",
+      fontSize: '1rem',
+      lineHeight: '2rem',
+      fontFamily: 'Mulish',
+      color: '#FFFFFF',
+      letterSpacing: '-0.01rem',
+      fontStyle: 'normal',
+      fontWeight: '200',
+      '@media (max-width:600px)': {
+        fontSize: '1rem',
         fontFamily: 'Mulish',
         fontWeight: 200,
         lineHeight: '1.2rem',
@@ -88,16 +88,16 @@ const themeClient = createTheme({
     },
     h5: {
       // Define the h5 typography settings
-      fontSize: "0.875rem",
+      fontSize: '0.875rem',
       fontWeight: 700,
-      fontFamily: "Lora",
-      letterSpacing: "-0.01rem",
-      color: "#0C111F",
-      textTransform: "capitalize",
-      lineHeight: "1.75 rem",
-      fontStyle: "normal",
-      "@media (max-width:600px)": {
-        fontSize: "0.875rem",
+      fontFamily: 'Lora',
+      letterSpacing: '-0.01rem',
+      color: '#0C111F',
+      textTransform: 'capitalize',
+      lineHeight: '1.75 rem',
+      fontStyle: 'normal',
+      '@media (max-width:600px)': {
+        fontSize: '0.875rem',
         fontFamily: 'Mulish',
         fontWeight: 200,
         lineHeight: '1.75rem',
@@ -107,46 +107,46 @@ const themeClient = createTheme({
     },
     h6: {
       // Define the h6 typography settings
-      fontSize: "0.75rem",
-      "@media (max-width:600px)": {
-        fontSize: "0.75rem",
+      fontSize: '0.75rem',
+      '@media (max-width:600px)': {
+        fontSize: '0.75rem',
       },
     },
     body1: {
       // Define the body1 typography settings
-      color: "#0C111F",
-      fontSize: "1.5rem",
+      color: '#0C111F',
+      fontSize: '1.5rem',
       fontWeight: 700,
-      fontFamily: "Lora",
-      lineHeight: "2.25rem",
-      letterSpacing: "0.01rem",
-      fontStyle: "normal",
-      "@media (max-width:600px)": {
-        fontSize: "1.5rem",
+      fontFamily: 'Lora',
+      lineHeight: '2.25rem',
+      letterSpacing: '0.01rem',
+      fontStyle: 'normal',
+      '@media (max-width:600px)': {
+        fontSize: '1.5rem',
       },
     },
     body2: {
       // Define the body2 typography settings
-      color: "#353535",
-      fontSize: "1.125rem",
-      lineHeight: "2.25rem",
+      color: '#353535',
+      fontSize: '1.125rem',
+      lineHeight: '2.25rem',
       fontWeight: 200,
-      fontFamily: "Mulish",
-      fontStyle: "normal",
-      letterSpacing: "-0.01rem",
-      "@media (max-width:600px)": {
-        fontSize: "1rem",
+      fontFamily: 'Mulish',
+      fontStyle: 'normal',
+      letterSpacing: '-0.01rem',
+      '@media (max-width:600px)': {
+        fontSize: '1rem',
       },
     },
     button: {
       // Define the button typography settings
-      textTransform: "capitalize",
+      textTransform: 'capitalize',
       fontWeight: 600,
-      color: "#C7923E",
-      fontFamily: "Mulish",
-      fontSize: "1rem",
-      "@media (max-width:600px)": {
-        fontSize: "0.75rem",
+      color: '#C7923E',
+      fontFamily: 'Mulish',
+      fontSize: '1rem',
+      '@media (max-width:600px)': {
+        fontSize: '0.75rem',
       },
     },
   },
@@ -157,16 +157,16 @@ const themeClient = createTheme({
       styleOverrides: {
         // Override the default styles
         root: {
-          borderRadius: "0.5rem", // Set the border radius
+          borderRadius: '0.5rem', // Set the border radius
         },
       },
     },
     MuiTab: {
       styleOverrides: {
         root: {
-          color: "#FFFFFF",
-          "&.Mui-selected": {
-            color: "#FFFFFF",
+          color: '#FFFFFF',
+          '&.Mui-selected': {
+            color: '#FFFFFF',
           },
         },
       },
