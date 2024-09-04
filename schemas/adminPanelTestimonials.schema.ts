5504f998b6e1203c7f0e39ae64291d93147f6c27
chithraka99 <chithrakanayana99@gmail.com>
2024-09-04 14:50:05 +0530

    Created the Admin Panel Testimonials Schema

---
+++ schemas/adminPanelTestimonials.schema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const schemaAdminPanelTestimonials = z.object({
+    profileImage: z
+        .array(z.instanceof(File))
+        .length(1, { message: "Profile image is required" }) 
+        .refine(files => files.every(file => file.size > 0), { message: "file must be non-empty" })
+        .refine(files => files.every(file => ["image/jpeg", "image/jpg", "image/png", "image/webp"].includes(file.type)), 
+        { message: "All files must be valid image types (JPEG, JPG, PNG, WEBP)" }),
+  
+    name: z
+        .string()
+        .nonempty('Enter the name.'),
+    
+    description: z
+        .string()
+        .nonempty('Enter the description.'),
+});
