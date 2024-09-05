cb455ad2da82b87deab39938edce2e63b8b6d66b
iamarasekera <iamarasekera.dev@gmail.com>
2024-09-05 19:13:21 +0800

    Updated the next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+            };
+        }
+        return config;
+    },
+    experimental: {
+        esmExternals: 'loose',
+    },
+}
 
 module.exports = nextConfig
