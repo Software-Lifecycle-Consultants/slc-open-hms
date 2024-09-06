4afa898b21de926affa97ace7429c3e8c48079bd
chithraka99 <chithrakanayana99@gmail.com>
2024-09-06 14:16:32 +0530

    Updated the Additional component

--- components/frontend/roomDetailsPage/Additional.tsx
+++ components/frontend/roomDetailsPage/Additional.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+"use client";
+import React from "react";
 import {
   Typography,
   Card,
@@ -9,44 +10,32 @@ import {
 } from "@mui/material";
 import { additional } from "@/data/roomDetails";
 import { Bolt } from "@mui/icons-material";
-import { schemaAdminPanelRoomDetailsAdditional } from "@/schemas/adminPanelRoomDetailsAdditional.schema";
-import { validateFormData } from "@/utils/validation";
 
-type AdditionalFormData = {
-  additionalInfoTitle: string;
-  additionalInfoDescription: string;
-}
-/* Functional component for ExtraFacilitation */
-const Additional = () => {
-  const [formData, setFormData] = useState<AdditionalFormData>({
-    additionalInfoTitle: "",
-    additionalInfoDescription: "",
-  });
-  const [additionalerrors, setadditionalerrors] = useState<Partial<AdditionalFormData>>({});
-  
-   // Handle form input change
-   const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-    setadditionalerrors({
-      ...additionalerrors,
-      [name]: "", // Reset error message for the field being changed
-    });
+interface AdditionalProps {
+  formAdditionalData: {
+    additionalInfoTitle: string;
+    additionalInfoDescription: string;
   };
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    const { errors: validationErrors, data } = validateFormData(schemaAdminPanelRoomDetailsAdditional, formData);
-  
-    if (validationErrors) {
-      setadditionalerrors(validationErrors);
-    } else {
-      console.log(data);
-    }
+  setAdditionalFormData: React.Dispatch<
+    React.SetStateAction<{
+      additionalInfoTitle: string;
+      additionalInfoDescription: string;
+    }>
+  >;
+  // Object containing validation error messages for form fields
+  errors: { [key: string]: string };
+}
+const Additional: React.FC<AdditionalProps> = ({
+  formAdditionalData,
+  setAdditionalFormData,
+  errors,
+}) => {
+  // Manages form input changes and updates the formData in the parent component.
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAdditionalFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   return (
@@ -55,55 +44,51 @@ const Additional = () => {
       <Card
         elevation={0}
         sx={{
-          height: "600px",
+          height: "630px",
           Width: "auto",
           border: "1px solid",
           borderRadius: "8",
           borderColor: "#D0D5DD",
           flexShrink: "0",
-        }}
-      >
+        }}>
         <Box>
-          <Typography
-            mt={2}
-            ml={2}
-          variant="h2"
-          >
+          <Typography mt={2} ml={2} variant="h2">
             {additional.additionalPageTitle}
           </Typography>
         </Box>
         <CardContent>
-          <form onSubmit={handleSubmit} noValidate>
+          <form>
             {/* Grid container for form layout */}
             <Grid container spacing={2}>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                 <TextField
                   fullWidth
-                  label={<b style={{ fontWeight: 'normal' }}>Enter Additional Information</b>}
+                  label={
+                    <b style={{ fontWeight: "normal" }}>
+                      Enter Additional Information
+                    </b>
+                  }
                   name="additionalInfoTitle"
-                  value={formData. additionalInfoTitle}
+                  value={formAdditionalData.additionalInfoTitle}
                   onChange={handleChange}
-                  error={!!additionalerrors. additionalInfoTitle}
-                  helperText={additionalerrors. additionalInfoTitle}
+                  error={!!errors.additionalInfoTitle}
+                  helperText={errors.additionalInfoTitle}
                 />
               </Grid>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Typography
-                  mb={2}
-                variant="h3"
-                >
+                <Typography mb={2} variant="h3">
                   Description
                 </Typography>
                 <TextField
                   fullWidth
                   multiline
-                  rows={16}
+                  rows={14}
                   label="Enter description"
                   name="additionalInfoDescription"
-                  value={formData.  additionalInfoDescription}
+                  value={formAdditionalData.additionalInfoDescription}
                   onChange={handleChange}
-                  error={!!additionalerrors.  additionalInfoDescription}
-                  helperText={additionalerrors.  additionalInfoDescription}
+                  error={!!errors.additionalInfoDescription}
+                  helperText={errors.additionalInfoDescription}
                 />
               </Grid>
             </Grid>
